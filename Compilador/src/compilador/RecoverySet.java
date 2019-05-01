bc5cec0984959577e7ad7cecaa946b81880235f0
Evelyn Rissi <erissi@edu.univali.br>
2019-05-01 20:17:57 -0300

    correcao

---
+++ Compilador/src/compilador/RecoverySet.java
@@ -0,0 +1,64 @@
+
+package compilador;
+
+import compilador.lexico.Parser;
+import java.util.HashSet;
+import java.util.Iterator;
+
+/**
+ *
+ * @author eerissi
+ */
+public class RecoverySet extends HashSet
+{
+
+    public RecoverySet()
+    {
+        super();
+    }
+
+    public RecoverySet(int t)
+    {
+        this.add(new Integer(t));
+    }
+
+    public boolean contains(int t)
+    {
+        return super.contains(new Integer(t));
+    }
+
+    public RecoverySet union(RecoverySet s)
+    {
+        RecoverySet t = null;
+        if (s != null)
+        {
+            t = (RecoverySet) this.clone();
+            t.addAll(s);
+        }
+
+        return t;
+    }
+
+    public RecoverySet remove(int n)
+    {
+        RecoverySet t = (RecoverySet) this.clone();
+        t.remove(new Integer(n));
+        return t;
+    }
+
+    public String toString()
+    {
+        Iterator it = this.iterator();
+        String s = "";
+        int k;
+
+        while (it.hasNext())
+        {
+            k = ((Integer) it.next());
+           // s += Parser.im(k) + " ";
+        }
+
+        return s;
+    }
+}
+
--- Compilador/src/compilador/SyntacticAnalyzer.java
+++ Compilador/src/compilador/SyntacticAnalyzer.java
@@ -31,4 +31,5 @@ public class SyntacticAnalyzer {
 		}
     	return output.toString();
     }
+    	
 }
